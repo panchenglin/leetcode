44a7c15cdc6e2b2c52f85e62f88faa2ebee946d9
chenlin <1299042716@qq.com>
2021-01-12 16:50:10 +0800

    广度优先 二叉树层级遍历

---
+++ src/com/chenlin/leetcode/tree/LevelOrderTree.java
@@ -0,0 +1,68 @@
+package com.chenlin.leetcode.tree;
+
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author bjpanhongwei
+ * @date 2021/1/12
+ **/
+public class LevelOrderTree {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        if (root == null) {
+            return ret;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<Integer>();
+            int currentLevelSize = queue.size();
+            for (int i = 1; i <= currentLevelSize; ++i) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            ret.add(level);
+        }
+
+        return ret;
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        //按层遍历即可
+        //1.
+        List<List<Integer>> lists = new ArrayList<>();
+        if (root == null) {
+            return lists;
+        }
+        //2.
+        List<TreeNode> nodes = new ArrayList<>();
+        nodes.add(root);
+        while (!nodes.isEmpty()) {
+            int size = nodes.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode remove = nodes.remove(0);
+                list.add(remove.val);
+                if (remove.left != null) {
+                    nodes.add(remove.left);
+                }
+                if (remove.right != null) {
+                    nodes.add(remove.right);
+                }
+            }
+            lists.add(list);
+        }
+        return lists;
+    }
+}
