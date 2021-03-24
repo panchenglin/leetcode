0a8b4802b0c45d4b77ee53d7d52a359a1d8ba8ab
chenlin <1299042716@qq.com>
2021-03-24 16:39:08 +0800

    19. 删除链表的倒数第 N 个结点

---
+++ src/com/chenlin/leetcode/linked/RemoveNthFromEnd.java
@@ -0,0 +1,80 @@
+package com.chenlin.leetcode.linked;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 19. 删除链表的倒数第 N 个结点
+ * 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+ * 进阶：你能尝试使用一趟扫描实现吗？
+ * https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/solution/shan-chu-lian-biao-de-dao-shu-di-nge-jie-dian-b-61/
+ * @author chenglin
+ * @date 2021/3/24
+ **/
+public class RemoveNthFromEnd {
+
+    /**
+     * 两次循环遍历
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        int length = getLength(head);
+        ListNode cur = dummy;
+        for (int i = 1; i < length - n + 1; ++i) {
+            cur = cur.next;
+        }
+        cur.next = cur.next.next;
+        ListNode ans = dummy.next;
+        return ans;
+    }
+
+    public int getLength(ListNode head) {
+        int length = 0;
+        while (head != null) {
+            ++length;
+            head = head.next;
+        }
+        return length;
+    }
+
+    /**
+     * 栈
+     */
+    public ListNode removeNthFromEndStrack(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        Deque<ListNode> stack = new LinkedList<ListNode>();
+        ListNode cur = dummy;
+        while (cur != null) {
+            stack.push(cur);
+            cur = cur.next;
+        }
+        for (int i = 0; i < n; ++i) {
+            stack.pop();
+        }
+        ListNode prev = stack.peek();
+        prev.next = prev.next.next;
+        ListNode ans = dummy.next;
+        return ans;
+    }
+
+    /**
+     * 双指针
+     */
+    public ListNode removeNthFromEnd2Pointer(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode first = head;
+        ListNode second = dummy;
+        for (int i = 0; i < n; ++i) {
+            first = first.next;
+        }
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        ListNode ans = dummy.next;
+        return ans;
+    }
+
+
+}
